ecf89f8ce59c6e7d0af5c3e31b625bb58724e405
seoldonghun <ehdgns5647@gmail.com>
2024-01-26 17:22:30 +0900

    cvPattern master

---
+++ src/test/java/com/example/cvpattern/CvpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cvpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvpatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
